735d42a9415583446c4eea59db1199b7b6483dfa
Antonio Ruiz <contacto.antonioruiz@gmail.com>
2021-08-10 19:19:17 +0200

    add searcher

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import React, { useState, useEffect } from 'react';
 
 import getGifs from './services/Api'
+import Gif from './components/Gif'
+import Search from './components/Search'
+
 import './App.css';
 
 function App() {
 
-  const [gifs, setGifs] = useState([]);
+  const [gifs, setGifs] = useState({"data":[]});
 
   useEffect(() => {
     getGifs({ keyword: 'f1' }).then(gifs => setGifs(gifs))
-  }, [])
+  }, []);
+
+  const GetSearch = (e) => {
+    if(e.target.value!=="")
+      getGifs({ keyword: e.target.value }).then(gifs => setGifs(gifs))
+    else
+      getGifs({ keyword: 'f1' }).then(gifs => setGifs(gifs))
+  }
 
   return (
     <div className="App">
+      <section> <Search mySearch={GetSearch}/> </section>
       <section className="App-content">
-        {
-          gifs.data.map((i) =>
-            <img src={i.images.downsized_medium.url} />
-          )}
+        { gifs.data.map((i) => 
+            <Gif key={i.id} url={i.images.downsized_medium.url} title={i.title}/>
+          ) 
+        }
       </section>
     </div>
   );
